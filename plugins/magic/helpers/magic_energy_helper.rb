40da9f3cc6be92df527c324f41090c674d5134a9
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2024-12-26 12:30:52 -0500

    Update magic_energy_helper.rb

--- plugins/magic/helpers/magic_energy_helper.rb
+++ plugins/magic/helpers/magic_energy_helper.rb
@@ -9,6 +9,7 @@ module AresMUSH
 
     def self.set_pc_energy(target, percent)
       Global.logger.debug percent
+      Global.logger.debug percent.to_i
       char = target
       percentage = percent.to_i / 100
       Global.logger.debug percentage
