12a7a996ae31fb19d2b87d0147fa55dd95e43923
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2024-12-26 10:20:14 -0500

    Update energy_restore_cmd.rb

--- plugins/magic/commands/energy_restore_cmd.rb
+++ plugins/magic/commands/energy_restore_cmd.rb
@@ -9,14 +9,13 @@ module AresMUSH
           args = cmd.parse_args(ArgParser.arg1_equals_arg2)
           self.target = Character.named(args.arg1)
           self.percent = trim_arg(args.arg2)
-          self.percent = self.percent.to_i
         end
   
         def check_errors
           return "You're not in combat." if !enactor.combat
-          return "Please specify a number between 1 and 100 for mana percentage." if self.percent.integer? == false
-          return "Please specify a number between 1 and 100 for mana percentage." if self.percent > 100
-          return "Please specify a number between 1 and 100 for mana percentage." if self.percent < 1
+          #return "Please specify a number between 1 and 100 for mana percentage." if self.percent.integer? == false
+          #return "Please specify a number between 1 and 100 for mana percentage." if self.percent > 100
+          #return "Please specify a number between 1 and 100 for mana percentage." if self.percent < 1
         end
   
         def handle
