08651f8aacdc0fe13312c9af8d725ba35b3ba78c
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2024-12-26 13:54:34 -0500

    Update energy_restore_cmd.rb

--- plugins/magic/commands/energy_restore_cmd.rb
+++ plugins/magic/commands/energy_restore_cmd.rb
@@ -7,7 +7,7 @@ module AresMUSH
 
         def parse_args
           args = cmd.parse_args(ArgParser.arg1_equals_arg2)
-          self.target = FS3Combat.find_named_thing(args.arg1)
+          self.target = FS3Combat.find_named_thing(args.arg1, enactor)
           self.percent = trim_arg(args.arg2)
         end
   
