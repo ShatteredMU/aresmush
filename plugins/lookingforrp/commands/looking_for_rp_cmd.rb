f63e7f3149954826ce59b356740b1862e600d5c7
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2025-01-08 16:02:15 -0500

    typo

--- plugins/lookingforrp/commands/looking_for_rp_cmd.rb
+++ plugins/lookingforrp/commands/looking_for_rp_cmd.rb
@@ -31,6 +31,7 @@ module AresMUSH
           LookingForRp.set(enactor, self.duration.to_i)
           if enactor.looking_for_rp_announce == true
             client.emit_success t('lookingforrp.set', :duration => self.duration)
+          end
           Channels.send_to_channel("RP Requests", t('lookingforrp.rp_request_emit', :name => enactor.name, :duration => self.duration))
         end
       end
