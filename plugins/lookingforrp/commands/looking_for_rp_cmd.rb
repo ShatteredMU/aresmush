934c5d0009ee0f697c5dd243777ea56dd89ca13d
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2025-01-09 09:43:42 -0500

    Lfrp emits (#26)
    * add emit to RP Requests
    
    * adding duration
    
    * add command to toggle announcements off
    
    * typo
    
    * typo
    
    * add command to lookingforrp.rb
    
    * remove unnecessary validation
    
    * add check to command
    
    * typo
    
    * tidying
    
    * add attr_accessor
    
    * trying to get the attr to show up
    
    * come on attr
    
    * boolean
    
    * goddang booleans
    
    * love of god
    
    * boolean GO BACK
    
    * get some defaults out there
    
    * eff booleans
    
    * love of god
    
    * i dont know ruby
    
    * wht
    
    * this is stupid
    
    * watch it work now
    
    * rubyyy
    
    * ruby why
    
    * going back
    
    * get IN THERE
    
    * ok
    
    * default pls
    
    * default PLS
    
    * git in there
    
    * Tidying and help file update
    
    * phrasing

--- plugins/lookingforrp/commands/looking_for_rp_cmd.rb
+++ plugins/lookingforrp/commands/looking_for_rp_cmd.rb
@@ -30,6 +30,9 @@ module AresMUSH
         else
           LookingForRp.set(enactor, self.duration.to_i)
           client.emit_success t('lookingforrp.set', :duration => self.duration)
+          if enactor.looking_for_rp_announce == "on"
+            Channels.send_to_channel("RP Requests", t('lookingforrp.rp_request_emit', :name => enactor.name, :duration => self.duration))
+          end
         end
       end
     end
