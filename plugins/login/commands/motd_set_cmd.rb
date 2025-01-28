93779a187272bfe6ae9cc12c722a86368fc93b7c
Nicole Arnold <aerrin99@gmail.com>
2025-01-28 16:06:25 -0500

    add motd to recent changes

--- plugins/login/commands/motd_set_cmd.rb
+++ plugins/login/commands/motd_set_cmd.rb
@@ -4,22 +4,29 @@ module AresMUSH
       include CommandHandler
 
       attr_accessor :notice
-      
+
       def parse_args
         self.notice = cmd.args
       end
-      
+
       def check_can_set
         return t('dispatcher.not_allowed')  if !Login.can_manage_login?(enactor)
         return nil
       end
-      
+
       def handle
         Game.master.update(login_motd: self.notice)
         client.emit_success t('login.motd_set')
         if (!self.notice.blank?)
           Manage.announce t('login.motd_announce', :enactor => enactor_name, :message => self.notice)
         end
+        Website.add_to_recent_changes(
+          'motd',
+          Website.format_input_for_html(self.notice),
+          { id: "motd-#{Time.now.strftime("%d/%m/%Y")}" },
+          enactor.name,
+          Website.format_input_for_html(self.notice)
+        )
       end
     end
   end
